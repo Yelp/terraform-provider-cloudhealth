d4db76cd08f9211f315d0e53f9b7185c97ebe57c
Sophie Matthews <smatthew@yelp.com>
2022-06-29 06:46:06 -0700

    Remove Terraform version namespace from package name

--- yelppack/itest.sh
+++ yelppack/itest.sh
@@ -3,14 +3,9 @@
 set -eu
 
 package=$1
-tfversion=$2
-version=$3
+version=$2
 
-if [[ ${tfversion} == "0.12" ]] ; then
-    install_path="/nail/opt/terraform-${tfversion}/bin/terraform-provider-cloudhealth"
-else
-    install_path="/usr/local/share/terraform/plugins/terraform-registry.yelpcorp.com/yelp/cloudhealth/${version}/linux_amd64/terraform-provider-cloudhealth"
-fi
+install_path="/usr/local/share/terraform/plugins/terraform-registry.yelpcorp.com/yelp/cloudhealth/${version}/linux_amd64/terraform-provider-cloudhealth"
 
 dpkg -i "$package"
 ls -la $install_path
